3810ddeb71c5322e3bd11966756c0a2b42ea9f6f
nlp-noob <jackyhu952@gmail.com>
2022-10-30 23:28:15 +0800

    safs

--- evaluation/get_similarity.py
+++ evaluation/get_similarity.py
@@ -1,6 +1,7 @@
 import pandas
+import torch
 
-def main():
+def get_test_data():
     # pearson kendall spearman 对应着三种方法
     # pandas.DataFrame.corr(method='pearson')
     text_data_path = "test_data/wordsim353_sim_rel/wordsim353_agreed.txt"
@@ -15,8 +16,20 @@ def main():
             continue
         a_pair = line.split('\t')
         a_pair[3] = float(str(a_pair[3][4:]))
-        word_pairs.append(line.split('\t'))
-    import pdb; pdb.set_trace()
+        word_pairs.append(a_pair)
+    return word_pairs
+
+def get_embeddings():
+
+
+
+def main():
+    folder = "../weights/cbow_Win5_BAT40_DIM300_Mul95.0/"
+    model = "191epochs_model.pt"
+    device = torch.device("cpu")
+    word_pairs = get_test_data()
+    pass
+
 
 if __name__=="__main__":
     main()
